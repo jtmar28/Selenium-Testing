f5f9cb647f422813f20b3f3a67bfbf9e689558dc
Justin Martinez <justinmartinez0123@gmail.com>
2021-03-26 15:17:03 -0500

    Auto complete forum

---
+++ Auto complete/test/Autocomplete.java
@@ -0,0 +1,27 @@
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.By;
+
+public class Autocomplete {
+    public static void main(String[] args) throws InterruptedException{
+
+        System.setProperty("webdriver.chrome.driver", "C:/Downloads/chromedriver_win32/chromedriver.exe");
+
+        WebDriver driver = new ChromeDriver();
+
+        driver.get("https://formy-project.herokuapp.com/autocomplete");
+
+        WebElement autocomplete = driver.findElement(By.id("autocomplete"));
+
+        //Find address
+        autocomplete.sendKeys("1555 Park Bldv, Palo Alto, CA");
+        //We have to do this because of time issue
+        Thread.sleep(1000);
+
+        WebElement autocompleteResult = driver.findElement(By.className("pac-item"));
+        autocompleteResult.click();
+
+        driver.quit();
+    }
+}
